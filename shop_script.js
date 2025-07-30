5e1303ad0dfe56bbc5dc925cafdb01dfc81aac3a
misaki-sensei <ict_nakayama@misaki.gse.okayama-c.ed.jp>
2025-07-30 14:16:48 +0900

    Update shop_script.js

--- shop_script.js
+++ shop_script.js
@@ -273,7 +273,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // QRコード描画エリアをリセット (canvasの内容をクリアし、qrCodeインスタンスもnullにする)
         if (qrCodeCanvas) {
             const oldCanvas = qrCodeCanvas;
-            const context = qrCodeCanvas.getContext('2d');
+            const context = qrCodeCanvas.getContext('canvas');
             if (context) {
                 context.clearRect(0, 0, qrCodeCanvas.width, qrCodeCanvas.height); // canvasの内容をクリア
             }
